c75c80aa957710b3b6a5df3c298bb52662d8fa61
vamshikrishna061 <kandagatlavamshikrishna11>
2023-05-04 00:09:16 +0530

    signup

---
+++ signup.js
@@ -0,0 +1,24 @@
+const signup = document.getElementById("signup");
+signup.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  let signupObj = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+    Number: document.getElementById("number").value,
+  };
+
+  axios
+    .post("hhtp://localhost:3000/user/signup", signupObj)
+    .then((response) => {
+      console.log(response.data.message);
+      alert(`User signup sucessfull`);
+      window.location.href = "./html/login";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
