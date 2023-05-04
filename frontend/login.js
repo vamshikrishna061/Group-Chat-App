11350fdc97fa46a1f3f7b0da55f10c39c31b565b
vamshikrishna061 <kandagatlavamshikrishna11>
2023-05-04 12:21:44 +0530

    Login Screen Frontend

---
+++ frontend/login.js
@@ -0,0 +1,26 @@
+const login = document.getElementById("login");
+login.addEventListener("submit", onSubmit);
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  loginObj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  axios
+    .post("http://localhost:3000/user/login", loginObj)
+    .then((response) => {
+      alert(response.data.message);
+    })
+    .catch((err) => {
+      console.log(err);
+      //document.body.innerHTML +=
+      //err +
+      // `<button onclick="window.location.href = '../html/login.html'">Reload</button>`;
+    });
+}
